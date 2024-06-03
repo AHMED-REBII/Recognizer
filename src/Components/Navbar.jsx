89b19f0494b9197be00f3dca5d6cdd88c3123dbc
Ahmed Rebii <ahmedrebai2001@gmail.com>
2024-06-03 17:14:22 +0100

    navbar modification

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = ({ open, setOpen }) => {
   ];
 
   return (
-    <div className=" w-full fixed top-0 left-0 ">
+    <div className=" w-full fixed top-0 left-0 bg-[#062b5a]">
       <div className="md:flex items-center justify-between py-4 md:px-10 px-7 ">
         <div className="font-bold text-3xl cursor-pointer flex items-center  text-white pt-2 ">
           <span className="w-16 h-16 mr-2 pt-2">
