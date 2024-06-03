f29082d06ca6ed472fdc8f8a03ba1f0b8a7270aa
Ahmed Rebii <ahmedrebai2001@gmail.com>
2024-06-03 17:35:43 +0100

    adjustment modification

--- src/Components/Upload.jsx
+++ src/Components/Upload.jsx
@@ -23,9 +23,9 @@ const Upload = () => {
     setSelected(option);
   };
   return (
-    <div className="flex flex-col items-center justify-center p-4   mt-4">
-      <div className="flex flex-col items-center justify-center  w-full md:w-4/6 h-80 md:h-5/6 border-4 border-dashed border-purple-500 rounded-lg md:rounded-2xl bg-[#031c42] cursor-pointer">
-        <div className="w-full md:rounded-xl bg-[#01132B] py-6 md:py-2 flex justify-around  items-center text-center md:justify-start">
+    <div className="flex flex-col items-center justify-center  p-7 mt-4">
+      <div className="flex flex-col items-center  w-full md:w-4/6 h-80 md:h-5/6 border-4 border-dashed border-purple-500 rounded-lg md:rounded-2xl bg-[#031c42] cursor-pointer ">
+        <div className="w-full md:rounded-xl bg-[#01132B] py-3 md:py-2 flex justify-evenly  items-center text-center md:justify-start">
           <div
             className={`flex flex-col items-center cursor-pointer md:mx-14 ${
               selected === "upload" ? "text-[#2EB1EE] underline" : "text-white"
@@ -74,14 +74,14 @@ const Upload = () => {
           <p className="mt-2 text-white text-lg">Drag and drop your image</p>
           <br />
           <p className="text-white text-lg">or</p>
-          <button className="mt-2 px-6 py-3 bg-[#2eb1ee] text-white rounded-full hover:bg-blue-400 transition-colors text-xl">
+          <button className="mt-2 px-6 py-3 bg-[#2eb1ee] text-white rounded-full hover:bg-blue-400 transition-colors text-xl ">
             Browse files
           </button>
         </div>
       </div>
 
       <button
-        className="mt-5 md:mt-7 px-8 py-4 bg-[#F57025] text-white rounded-full hover:bg-[#FFCB60] transition-colors text-xl"
+        className="mt-8 md:mt-8 px-8 py-4 bg-[#F57025] text-white rounded-full hover:bg-[#FFCB60] transition-colors text-xl"
         onClick={handleCompareClick}
       >
         Compare
