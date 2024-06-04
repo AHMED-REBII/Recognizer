34bd4cf2ed31c095b1f2832f870f4dbce773f7cb
Ahmed Rebii <ahmedrebai2001@gmail.com>
2024-06-04 14:28:26 +0100

    add the upload fonctionality

--- src/Components/Upload.jsx
+++ src/Components/Upload.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
+import { useDropzone } from "react-dropzone";
 import {
   IoCloudUploadOutline,
   IoCameraOutline,
@@ -9,8 +10,8 @@ import Loader from "./Loader";
 
 const Upload = () => {
   const [selected, setSelected] = useState("");
-
   const [view, setView] = useState("initial");
+  const [files, setFiles] = useState([]);
 
   const handleCompareClick = () => {
     setView("loading");
@@ -22,10 +23,23 @@ const Upload = () => {
   const handleSelection = (option) => {
     setSelected(option);
   };
+
+  const onDrop = useCallback((acceptedFiles) => {
+    setFiles(
+      acceptedFiles.map((file) =>
+        Object.assign(file, {
+          preview: URL.createObjectURL(file),
+        })
+      )
+    );
+  }, []);
+
+  const { getRootProps, getInputProps } = useDropzone({ onDrop });
+
   return (
-    <div className="flex flex-col items-center justify-center  p-7 mt-4">
-      <div className="flex flex-col items-center  w-full md:w-4/6 h-80 md:h-5/6 border-4 border-dashed border-purple-500 rounded-lg md:rounded-2xl bg-[#031c42] cursor-pointer ">
-        <div className="w-full md:rounded-xl bg-[#01132B] py-3 md:py-2 flex justify-evenly  items-center text-center md:justify-start">
+    <div className="flex flex-col items-center justify-center p-7 mt-4">
+      <div className="flex flex-col items-center w-full md:w-4/6 h-80 md:h-5/6 border-4 border-dashed border-purple-500 rounded-lg md:rounded-2xl bg-[#031c42] cursor-pointer">
+        <div className="w-full md:rounded-xl bg-[#01132B] py-3 md:py-2 flex justify-evenly items-center text-center md:justify-start">
           <div
             className={`flex flex-col items-center cursor-pointer md:mx-14 ${
               selected === "upload" ? "text-[#2EB1EE] underline" : "text-white"
@@ -34,13 +48,13 @@ const Upload = () => {
           >
             <IoDocumentAttachOutline
               className={`${
-                selected === "upload" ? "text-[#2EB1EE] " : "text-white"
+                selected === "upload" ? "text-[#2EB1EE]" : "text-white"
               } text-3xl`}
             />
             <span
               className={`${
                 selected === "upload"
-                  ? "text-[#2EB1EE] underline "
+                  ? "text-[#2EB1EE] underline"
                   : "text-white"
               } text-sm mt-1`}
             >
@@ -69,14 +83,46 @@ const Upload = () => {
             </span>
           </div>
         </div>
-        <div className="md:bg-[#3C5372] p-1 md:p-10 md:rounded-2xl  md:m-10 md:w-5/6 flex flex-col items-center justify-center">
-          <IoCloudUploadOutline className="text-white text-5xl mt-2 md:mt-5" />
-          <p className="mt-2 text-white text-lg">Drag and drop your image</p>
-          <br />
-          <p className="text-white text-lg">or</p>
-          <button className="mt-2 px-6 py-3 bg-[#2eb1ee] text-white rounded-full hover:bg-blue-400 transition-colors text-xl ">
-            Browse files
-          </button>
+        <div
+          {...getRootProps()}
+          className={`p-2 md:p-10 md:rounded-2xl md:m-10 md:w-5/6 flex flex-col items-center justify-center ${
+            files.length === 0 ? "md:bg-[#3C5372]" : ""
+          }`}
+        >
+          <input {...getInputProps()} />
+          {files.length === 0 ? (
+            <>
+              <IoCloudUploadOutline className="text-white text-5xl mt-2 md:mt-5" />
+              <p className="mt-2 text-white text-lg">
+                Drag and drop your image or PDF
+              </p>
+              <p className="text-white text-lg">or</p>
+              <button className="mt-2 px-6 py-3 bg-[#2eb1ee] text-white rounded-full hover:bg-blue-400 transition-colors text-xl">
+                Browse files
+              </button>
+            </>
+          ) : (
+            <>
+              <div className="flex flex-wrap  mt-4 md:mt-0">
+                {files.map((file) => (
+                  <div
+                    key={file.name}
+                    className="w-24 h-24 m-2 flex items-center justify-center bg-white rounded-lg md:w-64 md:h-64 "
+                  >
+                    <img
+                      src={file.preview}
+                      alt="preview"
+                      className="w-full h-full object-cover rounded-lg"
+                    />
+                    )
+                  </div>
+                ))}
+              </div>
+              <button className="mt-2 px-6 py-3 bg-[#2eb1ee] text-white rounded-full hover:bg-blue-400 transition-colors text-xl">
+                Browse files
+              </button>
+            </>
+          )}
         </div>
       </div>
 
